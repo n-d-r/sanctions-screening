2f1141c9a29362b6815a4e8153499255d1c39d03
n-d-r <crtl.ltrc.privat@googlemail.com>
2019-04-17 11:11:28 +0200

    added table definitions for non-individuals

--- ss_create_db_tables.sql
+++ ss_create_db_tables.sql
@@ -20,3 +20,36 @@ CREATE TABLE individuals(
   full_name_sorted TEXT,
   PRIMARY KEY(party_fixedref, profile_id, identity_id, alias_fixedref, name_id)
 );
+
+CREATE TABLE entities(
+  party_fixedref     TEXT,
+  profile_id         TEXT,
+  identity_id        TEXT,
+  alias_fixedref     TEXT,
+  name_id            TEXT,
+  entity_name        TEXT,
+  entity_name_sorted TEXT,
+  PRIMARY KEY(party_fixedref, profile_id, identity_id, alias_fixedref, name_id)
+);
+
+CREATE TABLE vessels(
+  party_fixedref     TEXT,
+  profile_id         TEXT,
+  identity_id        TEXT,
+  alias_fixedref     TEXT,
+  name_id            TEXT,
+  vessel_name        TEXT,
+  vessel_name_sorted TEXT,
+  PRIMARY KEY(party_fixedref, profile_id, identity_id, alias_fixedref, name_id)  
+);
+
+CREATE TABLE aircraft(
+  party_fixedref       TEXT,
+  profile_id           TEXT,
+  identity_id          TEXT,
+  alias_fixedref       TEXT,
+  name_id              TEXT,
+  aircraft_name        TEXT,
+  aircraft_name_sorted TEXT,
+  PRIMARY KEY(party_fixedref, profile_id, identity_id, alias_fixedref, name_id)  
+);
